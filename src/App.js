dae6778c0c482102a1ce6303f089f08cc97de5c8
Kyle Bomeisl <kaybomeisl2@gmail.com>
2024-05-06 22:13:46 -0400

    contrast and responsive layout

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import React, { useState } from "react";
 import {
     Button,
     Grommet,
-    Header, Menu,
+    Header,
     Page,
     PageContent,
     PageHeader,
     Text,
     Nav,
-    Anchor, Image, Avatar, Box, Card, CardHeader, CardBody, Carousel, Grid,
+    Anchor, Image, Avatar, Box, Card, CardHeader, CardBody, Carousel, Grid, CardFooter,
 } from "grommet";
-import {Moon, Sun, Home, Beacon, PieChart} from "grommet-icons";
+import {Moon, Sun, Home, Beacon, PieChart, Menu} from "grommet-icons";
 
 
 const theme = {
   global: {
       colors: {
           primary: '#616286',
+          darkNavy: '#0e1b29',
       },
     font: {
       family: "Roboto",
@@ -28,7 +29,7 @@ const theme = {
 
 const AppBar = (props) => (
     <Header
-      background="primary"
+      background="darkNavy"
       pad={{ left: "small", right: "small", vertical: "none" }}
       elevation="medium"
       {...props}
@@ -40,55 +41,66 @@ const App = () => {
   return (
       <Grommet theme={theme}>
         <Page>
+            <PageContent>
             <AppBar
 
+                pad={{ left: "small", right: "small", vertical: "xxsmall" }}
             >
-                <Card
+                <Box
                     direction="column"
                     pad="small"
-                    background="white"
-                    spacing={9}
-                    shadow="none"
-                    round="none"
+
                 >
                     <Avatar
                         alignSelf="center"
                         size="large"
-                        background={"light-1"}
+                        background={"white"}
                         src={"https://gravatar.com/avatar/8f65f7025001160d578dbf90e43f1998dfe23f35dc56fa6e33d0a967c5ae53ea"}
                     />
 
-                        <CardHeader>
+                        <CardHeader alignSelf='center'>
                             <Text
-                                color="text"
+                                color='white'
                                 alignSelf="center"
+                                size='large'
                             >
                                 Kyle Bomeisl
                             </Text>
                         </CardHeader>
 
-                    <CardBody>
+                    <CardBody alignSelf='center'>
+
                         <Text
-                            color="text-weak"
+                            color="white"
                             size="xsmall"
-                            alignSelf="center"
+                            alignSelf='center'
                         >
-                            Software Engineer (Ameriprise Financial) <br/>
+                            Software Engineer (Ameriprise Financial)
+                        </Text>
+                        <Text
+                            color="white"
+                            size="xsmall"
+                            alignSelf='center'
+                        >
                             Theoretical Condensed Matter Physics Researcher (Pixley Group)
                         </Text>
                     </CardBody>
 
-
-                </Card>
-                <Text size='large'>Theoretically Possible</Text>
+                </Box>
+                <Box direction='column' pad={{ left: "small", right: "small", vertical: "xxsmall" }}>
+                    <Text size='large' alignSelf='center'>Theoretically Possible</Text>
+                    <Text alignSelf='center'>A freemium series of physics and software engineering courses</Text>
+                </Box>
+                <Text size='medium'>Expertise is a Terrible Thing to Waste</Text>
             </AppBar>
-            <Header margin='none'>
-                <Box height="medium" width="medium" overflow="hidden" gridArea="carousel" margin='none'>
+            <Header margin='none' direction='row'>
+                <Box height="medium" width="xxlarge" overflow="hidden" margin='xxsmall' alignContent='center'>
                     <Carousel
                         margin="xsmall"
                         alignSelf="center"
                         play='4000'
-                        size="xxlarge"
+                        height='medium'
+                        width='large'
                         controls={false}
                     >
                         <Image fit="cover" src="https://live.staticflickr.com/65535/53704138164_dfb760dcb1.jpg" />
@@ -99,19 +111,8 @@ const App = () => {
                         <Image fit="cover" src="https://live.staticflickr.com/65535/53703874431_de63cb1a24_k.jpg" />
                     </Carousel>
                 </Box>
-                <Nav direction="row" background="primary" pad="medium">
-                    <Anchor icon={<Home />} hoverIndicator />
-                    <Anchor icon={<Beacon />} hoverIndicator />
-                    <Anchor icon={<PieChart />} hoverIndicator />
-                </Nav>
-
-
-
             </Header>
-
-            <PageContent>
-
-
+                
             </PageContent>
         </Page>
       </Grommet>
